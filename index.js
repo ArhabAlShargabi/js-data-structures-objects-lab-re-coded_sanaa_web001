d136bb5b9175f4d1e393abbd5f95ab9ce72e7239
Arhab Ahmed <arhab1000000@gmail.com>
2020-09-10 21:14:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// Write your solution in this file!
+//const driver = {};
+
 function updateDriverWithKeyAndValue(driver, key, value) {
   // Alternate using ES6 Spread operators:
   // return { ...driver, ...{ [key]: value } }
